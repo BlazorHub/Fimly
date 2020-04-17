152c7c85fcc348eee4beb92f5c0d091369ad4240
Beau Findlay <37879824+bdfin@users.noreply.github.com>
2020-04-17 21:10:38 +0100

    Add redirect to login component.

---
+++ Fimly/Components/RedirectToLogin.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Fimly.Components
+{
+    public class RedirectToLogin : ComponentBase
+    {
+        [Inject] protected NavigationManager NavigationManager { get; set; }
+
+        protected override void OnInitialized()
+        {
+            NavigationManager.NavigateTo("login");
+        }
+    }
+}
