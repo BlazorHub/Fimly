d4fde1b267eef34b626678674add6534d6690785
Beau Findlay <37879824+bdfin@users.noreply.github.com>
2020-04-17 20:14:56 +0100

    Add code-behind partial class to index page.

---
+++ Fimly/Pages/Index.razor.cs
@@ -0,0 +1,24 @@
+﻿using Fimly.Data.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Fimly.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        [Inject] AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+        [Inject] UserManager<AppUser> UserManager { get; set; }
+
+        protected AppUser CurrentUser;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+
+            CurrentUser = await UserManager.GetUserAsync(user);
+        }
+    }
+}
