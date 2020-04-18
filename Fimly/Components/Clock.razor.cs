ed3f562c41203286724da8df99fa373fe148f6b2
Beau Findlay <37879824+bdfin@users.noreply.github.com>
2020-04-18 18:24:12 +0100

    Add clock component.

---
+++ Fimly/Components/Clock.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Fimly.Components
+{
+    public partial class Clock : ComponentBase
+    {
+        private string Time { get; set; }
+
+        protected override void OnInitialized()
+        {
+            var timer = new System.Threading.Timer((_) =>
+            {
+                Time = DateTime.UtcNow.ToLocalTime().ToString();
+                InvokeAsync(() =>
+                {
+                    StateHasChanged();
+                });
+            }, null, 0, 1000);
+            base.OnInitialized();
+        }
+    }
+}
