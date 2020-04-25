03ce57c0c85a692220f6d042bbb6af3d54c659ae
Beau Findlay <37879824+bdfin@users.noreply.github.com>
2020-04-25 20:57:46 +0100

    Change tab control button styles.

--- Fimly/Components/ExpenseComponents/ExpenseCard.razor.cs
+++ Fimly/Components/ExpenseComponents/ExpenseCard.razor.cs
@@ -21,14 +21,15 @@ namespace Fimly.Components.ExpenseComponents
 
         List<Expense> Expenses => Person.Expenses.OrderByDescending(e => e.Cost).ToList();
 
-        List<Expense> ExpensesByCategory => (from ol in Person.Expenses
-                                             group ol by ol.ExpenseType.Name
-                                                into grp
-                                             select new Expense
-                                             {
-                                                 Name = grp.Select(ex => ex.ExpenseType.Name).FirstOrDefault(),
-                                                 Cost = grp.Sum(ex => ex.Cost)
-                                             }).ToList();
+        List<Expense> ExpensesByCategory =>
+            (from ol in Person.Expenses
+             group ol by ol.ExpenseType.Name
+                into category
+             select new Expense
+             {
+                 Name = category.Select(ex => ex.ExpenseType.Name).FirstOrDefault(),
+                 Cost = category.Sum(ex => ex.Cost)
+             }).ToList();
 
         string CurrentMonth => DateTime.Now.ToString("MMMM");
     }
